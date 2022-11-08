3a62e7c919be3a1b2068041ad2e7e1cf41a737cd
BesnikShala <77018193+BesnikShala@users.noreply.github.com>
2022-11-08 15:34:29 +0000

    Add Navbar to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import logo from '../src/assets/images/logo.png';
 import './App.css';
+import { NavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+
     </div>
   );
 }
