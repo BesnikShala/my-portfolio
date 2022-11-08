144f368c874a4cac7a000ed1bef3fb3d344ba90d
BesnikShala <77018193+BesnikShala@users.noreply.github.com>
2022-11-08 18:04:20 +0000

    Added navbar css and icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,55 +1,90 @@
 import { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from '../assets/images/logo.png';
-import { TiSocialLinkedinCircular, TiSocialGithubCircular } from 'react-icons/ti'
+import logo from "../assets/images/logo.png";
+import {
+  TiSocialLinkedinCircular,
+  TiSocialGithubCircular,
+} from "react-icons/ti";
 
 export const NavBar = () => {
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrolled] = useState(false);
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        }
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-        window.addEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll);
 
-        return () => window.removeEventListener("scroll", onScroll);
-    }, [])
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
-    }
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
 
-    return (
-    <Navbar bg="light" expand="lg" className={scrolled ? "scrolled": ""}>
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={logo} alt='logo' />
+          <img src={logo} alt="logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                <a href="https://www.linkedin.com/in/besnik-shala-a5045910a/" target="_blank">LinkedIn <TiSocialLinkedinCircular /></a>
-                <a href="https://github.com/BesnikShala" target="_blank">GitHub <TiSocialGithubCircular /></a>
+              <a
+                href="https://www.linkedin.com/in/besnik-shala-a5045910a/"
+                target="_blank"
+              >
+                <TiSocialLinkedinCircular style={{ color: "white" }} />
+              </a>
+              <a href="https://github.com/BesnikShala" target="_blank">
+                <TiSocialGithubCircular style={{ color: "white" }} />
+              </a>
             </div>
-            <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+            <button className="vvd" onClick={() => console.log("connect")}>
+              <span>Let's Connect</span>
+            </button>
           </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-
-}
+};
