0595fc224f48e7708d4f6c61bf6de29c05695a03
BesnikShala <77018193+BesnikShala@users.noreply.github.com>
2022-11-08 15:34:03 +0000

    Add Links and react icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from '../assets/images/logo.png';
+import { TiSocialLinkedinCircular, TiSocialGithubCircular } from 'react-icons/ti'
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -19,26 +21,29 @@ export const NavBar = () => {
         return () => window.removeEventListener("scroll", onScroll);
     }, [])
 
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+
     return (
     <Navbar bg="light" expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={''} alt='logo' />
+            <img src={logo} alt='logo' />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.link href="#projects">Projects</Nav.link>
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={''} alt="" /></a>
-                <a href="#"><img src={''} alt="" /></a>
-                <a href="#"><img src={''} alt="" /></a>
+                <a href="https://www.linkedin.com/in/besnik-shala-a5045910a/" target="_blank">LinkedIn <TiSocialLinkedinCircular /></a>
+                <a href="https://github.com/BesnikShala" target="_blank">GitHub <TiSocialGithubCircular /></a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
           </span>
