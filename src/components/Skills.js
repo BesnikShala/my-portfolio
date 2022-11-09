982c15f046fa5b718b132158e203374870517526
BesnikShala <77018193+BesnikShala@users.noreply.github.com>
2022-11-09 15:55:59 +0000

    Remove uneeded image and css

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,6 @@ import meter1 from "../assets/images/meter1.svg";
 import meter2 from "../assets/images/meter2.svg";
 import meter3 from "../assets/images/meter3.svg";
 import meter4 from "../assets/images/meter4.svg";
-import colorSharp from "../assets/images/color-sharp.png"
 
 export const Skills = () => {
     const responsive = {
