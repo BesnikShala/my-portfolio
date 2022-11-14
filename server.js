cc812687b5d6aeed7083211aa2820b1e2b3b1bf8
BesnikShala <77018193+BesnikShala@users.noreply.github.com>
2022-11-14 17:47:22 +0000

    made small amendements to white space and spacing

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.listen(5000, () => console.log("Server Running"));
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
+
+
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from '../src/assets/images/logo.png';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -16,7 +16,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
-
+      <Footer />
     </div>
   );
 }
