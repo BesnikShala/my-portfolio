c388477175174e8ab0aadf5f9e78ceb1707d218d
BesnikShala <77018193+BesnikShala@users.noreply.github.com>
2022-11-14 17:46:39 +0000

    Added Contact and Server.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,8 +22,24 @@ export const Contact = () => {
     });
   };
 
-  const handleSubmit = () => {
-    
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setButtonText('Sending...');
+    let response = await fetch("http://localhost:5000/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json;Charset=utf-8",
+        },
+        body:JSON.stringify(formDetails)
+    });
+    setButtonText("Send");
+    let result = await response.json();
+    setFormDetails(formInitialDetails);
+    if (result.code === 200) {
+        setStatus({ sucess: true, message: 'Message sent successfully'});
+    } else {
+        setStatus({ sucess: false, message: 'Something went wrong, please try again later.   ' })
+    }
   }
 
   return (
