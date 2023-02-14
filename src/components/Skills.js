2ccdb3de33065153f73470a17cd06b98d453b798
BesnikShala <77018193+BesnikShala@users.noreply.github.com>
2023-02-14 19:31:56 +0000

    Edit filler text

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -34,7 +34,7 @@ export const Skills = () => {
                     <Col>
                         <div className="skill-box">
                             <h2>Skills</h2>
-                            <p>Lorem Ipsum Text Lorem Ipsum Text Lorem Ipsum Text Lorem Ipsum Text</p>
+                            <p>My skills in Web Development</p>
                             <Carousel responsive={responsive} infinite={true} className="skill-slider">
                                 <div className='item'>
                                     <img src={meter1} alt="skill percentage meter" />
